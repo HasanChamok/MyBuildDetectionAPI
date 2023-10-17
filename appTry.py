7b8080885d602c1b3c6860efc5f8e4e6edc0f642
HasanChamok <hasan.chamok16@gmail.com>
2023-10-17 16:59:07 +0600

    Video and Image both detection done

--- appTry.py
+++ appTry.py
@@ -29,8 +29,8 @@ with open('classes.txt','r') as f:
     for line in f:
         classes.append(line.strip())
         
-def detect(img_path):
-    label , confidence = [] , []
+def detect_image(img_path):
+    label , confidence, Bbox = [] , [] , []
     
     img = cv.imread(img_path)
     img = cv.resize(img,(416,416))
@@ -42,11 +42,57 @@ def detect(img_path):
         for obj in range(len(res)):
             className = classes[res['class'][obj]]
             label.append(className)
+            (x1, y1, x2, y2) = (res['xmin'][obj],res['ymin'][obj],res['xmax'][obj],res['ymax'][obj])
+            bbox=(x1,y1,x2,y2)
+            Bbox.append(bbox)
             confidence.append(res['confidence'][obj])
     
-    return label , confidence 
+    return label , confidence, Bbox 
 
-
+def detect_video(vid_path):
+    cap = cv.VideoCapture(vid_path)
+    
+    frames = []
+    
+    while True:
+        ret , frame = cap.read()
+        if not ret:
+            break
+        frames.append(frame)
+    cap.release()
+    
+    frame_no = 0
+    results = []
+    
+    for frame in frames:
+        # results.append(model(frame).pandas().xyxy[0])
+        detected = model(frame)
+        labels, confidences, Bbox = [], [], []
+        for res in detected.pandas().xyxy:
+            for obj in range(len(res)):
+                if res['confidence'][obj] > 0.1:  # Confidence threshold
+                    label = classes[res['class'][obj]]
+                    labels.append(label)
+                    confidence = res['confidence'][obj]
+                    confidences.append(confidence)
+                    (x1, y1, x2, y2) = (res['xmin'][obj],res['ymin'][obj],res['xmax'][obj],res['ymax'][obj])
+                    bbox=(x1,y1,x2,y2)
+                    Bbox.append(bbox)
+                    # Create a dictionary for the current detection result
+                    detection_result = {
+                        "Frame No" : frame_no,
+                        "label": label,
+                        "confidence": confidence
+                        # "bbox": bbox
+                    }
+                    results.append(detection_result)
+                    
+        frame_no+=1
+    # print(results)
+    # print(len(results))
+    # results = []
+    
+    return results
 
 @app.route('/upload', methods=['GET','POST'])
 
@@ -61,32 +107,24 @@ def upload_file():
         
         if file.filename == '':
             return jsonify({'error': 'No selected file'})
-        # img = request.files['image']
-        # vid = request.files['video']
         
-        # if img.filename == '' and vid.filename == '':
-        #     return 'No selected files'
-        
-        #Handle image files
-        # if img.filename != '':
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return jsonify({'message': 'File uploaded successfully', 'filename': filename})
+            filePath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(filePath)
+            # return jsonify({'message': 'File uploaded successfully', 'filename': filename})
+            if filename.lower().endswith(('png','jpg','jpeg','gif')):
+                label, confidence, Bbox = detect_image(filePath)
+                return jsonify({"Class": label},{"Bounding Box": Bbox},{"Confidence Score":confidence})
+            elif filename.lower().endswith(('.mp4', '.avi', '.mov')):
+                results = detect_video(filePath)
+                return jsonify({'Results ':  results})
         else:
             return jsonify({'error': 'Invalid file format'})
         
-        # img_name = secure_filename(file.filename)
-        # img_path = 'saved/' + img_name
-        # img.save(img_path)
+        # label, confidence = detect_image(filePath)
         
-        # img = cv.imread(img_path)
-        # img = cv.resize(img,(416,416))
-        
-        # label, confidence = detect(img_path)
-            # label , confidence = ['BALL'] , ["0.56"]
-            
-    return jsonify({"Class": label},{"Confidence Score":confidence})
+    # return jsonify({"Class": label},{"Confidence Score":confidence})
         
         
 
